12dae6ab16fa65d1435aff7753467ace3a8ea647
lorenzoarlo <arlotti.lorenzo2@gmail.com>
2023-05-28 00:19:08 +0200

    another fix

--- lacb.js
+++ lacb.js
@@ -222,7 +222,3 @@ class lacb extends HTMLElement {
 }
 
 document.currentScript.onload = function() { if(!customElements.get("lorenzoarlo-cookiebutton")) customElements.define("lorenzoarlo-cookiebutton", lacb);};
-
-if(!customElements.get("lorenzoarlo-cookiebutton")) customElements.define("lorenzoarlo-cookiebutton", lacb);
-
-console.log("evalueted!");
